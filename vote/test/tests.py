1a4a0a85474369f3534a826a4e9e90b6ae166aea
Carolina <1700250@ucc.edu.ar>
2020-11-24 20:21:02 -0300

    app test added

---
+++ vote/test/tests.py
@@ -0,0 +1,19 @@
+import requests
+import unittest
+
+
+class UnitTest(unittest.TestCase):
+    url = 'https://tp-vote.herokuapp.com/'
+
+    def test_get_status_200(self):
+        response = requests.get(self.url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_incomplete_vote_status_400(self):
+        response = requests.post(self.url)
+        self.assertEqual(response.status_code, 400)  # Bad Req
+
+    def test_set_new_cookie(self):
+        payload = {'vote': 'a'}
+        response = requests.post(self.url, data=payload)
+        assert response.cookies["voter_id"] is not None
