5d286e7556d318986a52204decb23d7ceb01d812
Carolina <1700250@ucc.edu.ar>
2020-11-24 01:24:38 -0300

    app fixing

--- vote/app.py
+++ vote/app.py
@@ -4,36 +4,31 @@ import os
 import socket
 import random
 import json
-import urlparse
 
-option_a = os.getenv('OPTION_A', "Cows")
+option_a = os.getenv('OPTION_A', "Cats")
 option_b = os.getenv('OPTION_B', "Dogs")
 hostname = socket.gethostname()
 
-url = urlparse.urlparse(os.environ.get('REDISCLOUD_URL'))
-
 app = Flask(__name__)
 
-
-def get_redis(host, port, password):
+def get_redis():
     if not hasattr(g, 'redis'):
-        try:
-            g.redis = Redis(
-                host=host,
-                port=port,
-                password=password,
-                socket_timeout=5)
-        except Exception as ex:
-            print('Error:', ex)
-            g.redis = None
+        g.redis = Redis(os.getenv('REDIS_URI'), db=0, socket_timeout=5)
     return g.redis
 
-
-@app.route("/", methods=['POST', 'GET'])
+@app.route("/", methods=['POST','GET'])
 def hello():
-    voter_id = get_voter(request.cookies.get('voter_id'))
+    voter_id = request.cookies.get('voter_id')
+    if not voter_id:
+        voter_id = hex(random.getrandbits(64))[2:-1]
 
-    vote = count_vote(None, voter_id)
+    vote = None
+
+    if request.method == 'POST':
+        redis = get_redis()
+        vote = request.form['vote']
+        data = json.dumps({'voter_id': voter_id, 'vote': vote})
+        redis.rpush('votes', data)
 
     resp = make_response(render_template(
         'index.html',
@@ -46,21 +41,5 @@ def hello():
     return resp
 
 
-def count_vote(vote, voter_id):
-    if request.method == 'POST':
-        redis = get_redis(host=url.hostname, port=url.port, password=url.password)
-        vote = request.form['vote']
-        data = json.dumps({'voter_id': voter_id, 'vote': vote})
-        redis.rpush('votes', data)
-    return vote
-
-
-def get_voter(voter_id):
-    if not voter_id:
-        print('Usuario nuevo')
-        voter_id = hex(random.getrandbits(64))[2:-1]
-    return voter_id
-
-
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', threaded=True, port=url.port)
+    app.run(host='0.0.0.0', port=80, debug=True, threaded=True)
