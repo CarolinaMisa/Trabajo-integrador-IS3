2bcf831cb88b4b3b80b5a9e256f96c319f726379
Carolina <1700250@ucc.edu.ar>
2020-11-24 01:29:38 -0300

    app.py backward

--- vote/app.py
+++ vote/app.py
@@ -5,30 +5,35 @@ import socket
 import random
 import json
 
-option_a = os.getenv('OPTION_A', "Cats")
+option_a = os.getenv('OPTION_A', "Cows")
 option_b = os.getenv('OPTION_B', "Dogs")
 hostname = socket.gethostname()
 
+PORT = int(os.environ.get("PORT", 5000))
+REDIS_PORT = os.environ.get('REDIS_PORT', '5000') 
+REDIS_HOST = os.environ.get('REDIS_HOST', 'redis')
+REDIS_PASSWORD = os.environ.get('REDIS_PASSWORD', 'pass')
+
 app = Flask(__name__)
 
-def get_redis():
+def get_redis(REDIS_HOST, REDIS_PASSWORD, REDIS_PORT):
     if not hasattr(g, 'redis'):
-        g.redis = Redis(os.getenv('REDIS_URI'), db=0, socket_timeout=5)
+        try:
+            g.redis = Redis(
+                host=REDIS_HOST, 
+                port=REDIS_PORT,
+                password=REDIS_PASSWORD,
+                socket_timeout=5)
+        except Exception as ex:
+            print('Error:', ex)
+            g.redis = None
     return g.redis
 
-@app.route("/", methods=['POST','GET'])
+@app.route("/", methods=['POST', 'GET'])
 def hello():
-    voter_id = request.cookies.get('voter_id')
-    if not voter_id:
-        voter_id = hex(random.getrandbits(64))[2:-1]
+    voter_id = get_voter(request.cookies.get('voter_id'))
 
-    vote = None
-
-    if request.method == 'POST':
-        redis = get_redis()
-        vote = request.form['vote']
-        data = json.dumps({'voter_id': voter_id, 'vote': vote})
-        redis.rpush('votes', data)
+    vote = count_vote(None, voter_id)
 
     resp = make_response(render_template(
         'index.html',
@@ -40,6 +45,20 @@ def hello():
     resp.set_cookie('voter_id', voter_id)
     return resp
 
+def count_vote(vote, voter_id):
+    if request.method == 'POST':
+        redis = get_redis(REDIS_HOST=REDIS_HOST, REDIS_PASSWORD=REDIS_PASSWORD, REDIS_PORT=REDIS_PORT)
+        vote = request.form['vote']
+        data = json.dumps({'voter_id': voter_id, 'vote': vote})
+        redis.rpush('votes', data)
+    return vote
+
+def get_voter(voter_id):
+    if not voter_id:
+        print('Usuario nuevo')
+        voter_id = hex(random.getrandbits(64))[2:-1]
+    return voter_id
+
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80, debug=True, threaded=True)
+    app.run(host='0.0.0.0', threaded=True, port=PORT)
